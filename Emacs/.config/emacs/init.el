95d50e8870c76238658a8ee42e13d519289188dd
Liam Mulhall <liammulh@gmail.com>
2026-03-28 14:38:17 -0700

    Add existing dotfiles

---
+++ Emacs/.config/emacs/init.el
@@ -0,0 +1,45 @@
+;;=====================================================================
+;; MELPA
+;;=====================================================================
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;;=====================================================================
+;; Theme
+;;=====================================================================
+(unless (package-installed-p 'gruvbox-theme)
+  (package-refresh-contents)
+  (package-install 'gruvbox-theme))
+(load-theme 'gruvbox-dark-medium t)
+
+;;=====================================================================
+;; Font
+;;=====================================================================
+(set-frame-font "DepartureMono Nerd Font Mono" nil t) 
+
+;;=====================================================================
+;; GUI Elements
+;;=====================================================================
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+;;=====================================================================
+;; Vertical Ruler
+;;=====================================================================
+(setq display-fill-column-indicator-column 72)
+(display-fill-column-indicator-mode 1)
+
+;;=====================================================================
+;; Org Mode
+;;=====================================================================
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;;=====================================================================
+;; Custom Variables
+;;=====================================================================
+(custom-set-variables
+  '(org-agenda-files (quote ("~/Code/Notes/"))))
---
+++ README.md
@@ -0,0 +1,32 @@
+Dotfiles
+=======================================================================
+
+This repository houses the configurations for the software I use.
+
+Getting Started
+-----------------------------------------------------------------------
+
+- Install [GNU Stow](https://www.gnu.org/software/stow/).
+- Install whatever software you're interested in, e.g. Vim.
+- Use Stow to create symbolically linked files in the appropriate
+  locations: `stow Vim`.
+
+Using This Repository
+-----------------------------------------------------------------------
+
+Each piece of software gets its own directory. Within that directory,
+create the same directories and files that you would normally expect
+in your home directory. For example, suppose you want to store your
+Emacs configuration in `~/.config/emacs/init.el`, then in this
+repository, create the following structure:
+
+```
+Emacs/
+  |_.config/
+    |_emacs/
+      |_init.el
+```
+
+Then when you've installed Emacs, use `stow Emacs` to create a symlink
+file in `~/.config/emacs/init.el` that points to the file in this
+repository.
